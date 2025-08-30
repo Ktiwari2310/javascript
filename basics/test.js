87f770a9f58ce2bd94d70ec67312b80e5ce59d44
Ktiwari2310 <beingkt.23@gmail.com>
2025-08-30 12:00:37 +0000

    Array1

--- basics/test.js
+++ basics/test.js
@@ -1,2 +1,76 @@
-console.log("Khushboo")
-console.log("Vivek")
+
+// Arrays
+const a = [1,8,2,3,9,true,"Khushboo"];
+const myheros=[ "Shaktiman", "IronMan","Superman"];
+const a2= new Array(1,6,8,9,7,4);
+// console.log(a[6]);
+// Methods
+/*
+
+console.log(a2.length); // gives length of array
+
+a.push(8); // push 8 to the array
+console.log(a); // [ 1, 8, 2, 3, 9, true, 'Khushboo', 8 ]
+
+a.pop();
+console.log(a); //[ 1, 8, 2, 3, 9, true, 'Khushboo' ]
+a.pop();
+console.log(a); //[ 1, 8, 2, 3, 9, true ]
+
+a.unshift(9);  // push 9 at initail
+console.log(a); 
+
+
+a.unshift();  // pop initial element
+console.log(a);
+
+
+console.log(a.includes(5)); // True or false based on ques   --> False as 5 is not present boolean type
+console.log(a.indexOf(9)); // return 4 and if elementsis not there will retutn -1
+
+const a3 = a.join();
+console.log(a);
+console.log(a3);
+// output is: [ 1, 8, 2, 3, 9, true, 'Khushboo' ] as Array type
+//            1,8,2,3,9,true,Khushboo as String type
+console.log(typeof a3);    //string
+
+console.log("A", a)  //A [ 1, 8, 2, 3, 9, true, 'Khushboo' ]
+const a4 = a.slice(1,6)
+console.log(a4)  //[ 8, 2, 3, 9, true ]
+console.log("b", a)  //b [ 1, 8, 2, 3, 9, true, 'Khushboo' ]
+
+
+console.log("A", a)   //A [ 1, 8, 2, 3, 9, true, 'Khushboo' ]
+const a4 = a.splice(1,6) //[ 8, 2, 3, 9, true, 'Khushboo' ]
+console.log(a4) 
+console.log("B", a)  //B [ 1 ]
+
+// Slice--> no effect on original array and range not included
+// Splice--> affect original array and range is included
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
