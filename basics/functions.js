7bcaedc5f2f815e6536db7e3076f426fec33fce4
Ktiwari2310 <beingkt.23@gmail.com>
2025-09-27 17:25:15 +0530

    functions

--- basics/functions.js
+++ basics/functions.js
@@ -13,7 +13,7 @@
 // function to add to numbers
 
 function add(num1,num2) {
-    console.log(num1+num2);
+    // console.log(num1+num2);
 }
 
 add(5,6); 
@@ -24,11 +24,63 @@ function subtract(num1,num2) {
 
 const res = subtract(11,9)
 // console.log(res);
-console.log("Result : ", res);
+// console.log("Result : ", res);
 
 function loggedin(username = "Guest"){
     return `${username} just logged in`
 }
 
-console.log(loggedin());   // no argument the takes default "Guest"
+// console.log(loggedin());   // no argument the takes default "Guest"
 
+// for eg you have a cart, you have no idea how many items customer will be adding and you need to add it
+
+function calculatecartprice(...num1){  //... is rest
+    return num1;
+    
+}
+ console.log(calculatecartprice(200, 500, 7000));   //[ 200, 500, 7000 ]  return as array
+
+
+
+//  function calculatecartprice(val1, val2, ...num1){  //... is rest
+//     return num1;
+    
+// }
+//  console.log(calculatecartprice(200, 500, 7000)); // only [7000]
+
+
+const user = {
+    username : "Khushboo",
+    price : 500
+
+}
+
+function handleobject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+
+}
+
+handleobject(user)
+handleobject({
+    username: "KhushbooTiwari",
+    price : 499
+})
+// we have passed object now lets pass array
+
+const myarray =[200,400, 600]
+
+function handlearray(getarray){
+  
+    return getarray[1]+ getarray[0]+getarray[2]
+    
+}
+
+// console.log(handlearray(myarray));
+// console.log(handlearray([200,1000,5000]));
+
+
+console.log(handlearray(calculatecartprice(200, 500, 7000)));  // passing function in function
+
+
+
+ 
