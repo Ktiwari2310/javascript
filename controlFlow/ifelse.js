18d4b90f7be22bb9fe4500c98ed9f43fbc1365e8
Ktiwari2310 <beingkt.23@gmail.com>
2025-10-04 20:02:04 +0530

    Control Flow

---
+++ controlFlow/ifelse.js
@@ -0,0 +1,57 @@
+// if 
+
+// if(2==2){
+//     //code will be executed
+//     console.log("yes");
+// }
+
+// if(false){
+//     //codw won't be executed
+// }
+
+//  operators : <,>,<=,>=,==,!=,===(type checking),!==
+
+// const temp= 55
+// if(temp<50){
+//     console.log("Temperature is less than 50");
+    
+// }
+// else {
+//      console.log("It is not less than 50");
+// }
+
+
+// const balance= 1000
+
+// if (balance<500) {
+//     console.log("less than 500");
+        
+// } else if(balance<750){
+//     console.log("less than 750");
+
+// } else if(balance<900){
+//     console.log("less than 900");
+// }
+// else{
+//     console.log("less than 1200");
+// }
+   
+const isloggedin= true
+const debitCard = false
+const loggedingoogle = true
+const loggedinemail= false
+
+// if(isloggedin && debitCard){
+//     console.log("Shopping is allowed");
+    
+// }else{
+//     console.log("Shopping is not allowed");   
+// }
+
+// if(loggedinemail || loggedingoogle){
+//     console.log("User logged in");
+    
+// }else{
+//     console.log("User is not logged in");   
+// }
+
---
+++ controlFlow/truthy.js
@@ -0,0 +1,66 @@
+// const useremail = "ktiwari@gmail.com"
+const useremail =[]
+
+// if(useremail){
+//     console.log("Got user email");  //print this
+    
+// } else {
+//     console.log("Don't have user email");
+    
+// }
+
+// if(!useremail){
+//     console.log("Got user email");
+    
+// } else {
+//     console.log("Don't have user email");  //print this
+    
+// }
+
+// falsy values 
+
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+
+// truthy values
+
+// true, "0", 'false', " ", [], {}, function(){}
+
+// if (useremail.length ===0) {
+//     console.log("Array is empty");
+    
+    
+// }
+
+// const user = { }
+//  if (Object.keys(user).length==0) {
+//     console.log("Object is empty");
+    
+    
+//  }
+//  else {
+//     console.log("Hi");
+    
+//  }
+
+
+// false ==0 -->true
+// false == '0'-->true
+// 0 == ''-->true
+
+// Nullish Coalescing Operator (??): null undefined
+
+// let val;
+// val=5 ?? 10
+//  val=null ?? 10  // prints 10
+// val= undefined ?? 10  // prints 10
+// val = null ?? 10 ?? 20
+// console.log(val);
+
+// terniary operator
+// condition ?true : false
+
+3<2 ? console.log("true") : console.log("False");
+
+
+
