e8444908660fdf00d446a453a373fe92d0b4ccee
Ktiwari2310 <beingkt.23@gmail.com>
2025-10-20 18:08:59 +0530

    loops

---
+++ loops/Highorderloop.js
@@ -0,0 +1,126 @@
+//for of loop
+
+
+const arr= [1,2,3,4,5]
+
+for (const val of arr) {
+    // console.log(val);   
+}
+
+const greeting = "Good morning"
+
+for (const val of greeting) {
+    if(val==" "){
+        continue   //removes the space detected
+    }
+    // console.log(val);
+    
+}
+
+//maps --> unique
+
+const map = new Map()
+map.set('IN',"India")
+map.set('USA',"United States of America")
+map.set('UK',"United Kingdom")
+
+// console.log(map);
+
+
+// for (const [key,value] of map) {
+// //    console.log(key);
+//    // op
+// //    [ 'IN', 'India' ]
+// // [ 'USA', 'United States of America' ]
+// // [ 'UK', 'United Kingdom' ]
+//     // console.log(key, '->',value);
+
+// }
+
+
+
+const myobj= {
+    game1: "NFS",
+    game2: "Spiderman"
+}
+
+// for (const [key,value] of myobj) {
+//     console.log(key, '->',value);
+// }
+//this is not iterable , for objects we have other method
+
+
+// for (const key in myobj) {
+// //    console.log(`${key} is ${myobj[key]}`);
+   
+// }
+
+
+// const programming =[ "js","cpp","java"]
+
+// for (const key in programming) {
+   
+//     console.log(programming[key]);
+    
+    
+// }
+
+// for (const [key,value] in map) {
+//   console.log(key,value);
+   
+// }
+//no output since it is not iterable
+
+
+
+//for each loop
+
+const coding = ["java","cpp","js","ruby"]
+
+// coding.forEach( function (item) {
+//     console.log(item);
+    
+// })
+
+// coding.forEach( (val) => {
+//     console.log(val);
+    
+// })
+
+// function printme(item){
+//     console.log(item);
+    
+// }
+// coding.forEach(printme)
+
+coding.forEach((item,index,arr)=>{
+    // console.log(item,index,arr);
+    
+})
+// o/p
+// java 0 [ 'java', 'cpp', 'js', 'ruby' ]
+// cpp 1 [ 'java', 'cpp', 'js', 'ruby' ]
+// js 2 [ 'java', 'cpp', 'js', 'ruby' ]
+// ruby 3 [ 'java', 'cpp', 'js', 'ruby' ]
+
+
+const mycoding = [
+    {
+        lang_name : "js",
+        lang_file:"js1"
+    },
+     {
+        lang_name : "cpp",
+        lang_file:"cpp1"
+    },
+     {
+        lang_name : "java",
+        lang_file:"java1"
+    }
+]
+
+mycoding.forEach((item)=>{
+
+    console.log(item.lang_file);
+    
+})
---
+++ loops/while.js
@@ -0,0 +1,23 @@
+// let i=0
+// while (i<5) {
+//     console.log(`The value of i is ${i}`);
+//     i++
+    
+// }
+
+// let myarr=["flash","superman","ironman"]
+// let arr=0
+// while (arr<myarr.length) {
+//     console.log(`Array elements are ${myarr[arr]}`);
+//     arr++
+// }
+
+let n=1
+do {
+    // console.log("Khushboo");
+    n++
+} while (n<=5);
+
+
+
+
